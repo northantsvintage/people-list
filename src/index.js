32c06cf752bfcbf74e44931d37654dd775a9a618
northantsvintage <northantsvintage@gmail.com>
2020-08-18 15:24:45 +0100

    css and dynamic props

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";
 
-const Person = () => {
+const Person = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/med/men/${img}.jpg`;
   return (
-    <article>
-      <img src="https://randomuser.me/api/portraits/med/men/75.jpg" />
-      <h4>name</h4>
-      <h6>job</h6>
+    <article className="person">
+      <img src={url} alt="profile" />
+      <h4>{name}</h4>
+      <h6>{job}</h6>
+      {children}
     </article>
   );
 };
 
 const PersonList = () => {
   return (
-    <section>
-      <Person />
+    <section className="personList">
+      <Person img="7" name="John" job="developer" />
+      <Person img="27" name="Bob" job="designer">
+        <p>
+          Found a bug or have an idea? Contribute to randomuser.me's database on
+          github.com
+        </p>
+      </Person>
+      <Person img="37" name="Nick" job="cleaner" />
     </section>
   );
 };
